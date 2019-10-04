92f7c11d798a056dd2855586c4e01634bb88cf25
tpeea <adufosse43@live.fr>
2019-10-04 11:55:51 +0200

    Ma première sauvegarde

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Slider mSlider; // Création du slider
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //On va instancier le slider
+        mSlider = new Slider(this);
+
     }
 }
 
--- app/src/main/java/com/example/myapplication/Slider.java
+++ app/src/main/java/com/example/myapplication/Slider.java
@@ -1,12 +1,15 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.jar.Attributes;
 
@@ -55,18 +58,89 @@ public class Slider extends View {
     }
 
     private void init(Context context, AttributeSet attributeSet) {
+
         //Initialisation des dimensions par défaut en pixel
-        mBarLenght = dpToPixel (DEFAULT_BAR_LENGTH);
+        mBarLenght = dpToPixel (DEFAULT_BAR_LENGTH); // Le rouge des fonctions se fini quand la fonction est déclaré en bas
         mCursorDiameter = dpToPixel (DEFAULT_CURSOR_DIAMETER);
         mBarWidth = dpToPixel (DEFAULT_BAR_WIDTH);
+
+        // On instancie les variables Paint
+        mCursorPaint = new Paint();
+        mBarPaint = new Paint();
+        mValueBarPaint = new Paint();
+
+        // On empêche aliasing (repliement) On limite les hautes fréquences dans les trucs graphiques
+        mCursorPaint.setAntiAlias(true);
+        mBarPaint.setAntiAlias(true);
+        mValueBarPaint.setAntiAlias(true);
+
+        //Il va falloir définir nos pinceaux ==> Application du style (plein)
+        mValueBarPaint.setStyle(Paint.Style.STROKE); //Le contour de notre objet
+        mBarPaint.setStyle(Paint.Style.STROKE);
+        mCursorPaint.setStyle(Paint.Style.FILL_AND_STROKE); //Contour et remplissage intérieur
+
+        //Petite astuce pour avoir un trait arrondis en haut et en bas
+        mBarPaint.setStrokeCap(Paint.Cap.ROUND);
+        mValueBarPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        //Couleur par défaut si on va voir dans res ==> Values et qu'on ouvre le colors.xml on a les couleurs
+        mDisabledColor = ContextCompat.getColor(context,R.color.colorDisabled);
+
+        //fixe les largeurs
+        mBarPaint.setStrokeWidth(mBarWidth);
+        mValueBarPaint.setStrokeWidth(mBarWidth);
+
+        //On initialise les dimensions minimales en pixel
+        int minWidth = (int) dpToPixel(MIN_CURSOR_DIAMETER)+ getPaddingLeft() + getPaddingRight();
+        int minHeight = (int) dpToPixel(MIN_BAR_LENGTH + MIN_CURSOR_DIAMETER)+ getPaddingBottom() + getPaddingTop();
+
+        setMinimumHeight(minHeight);
+        setMinimumWidth(minWidth);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //Pour pouvoir faire notre tracé à l'écran, on va s'aider de point. Drawline = fait une ligne et va falloir lui donner une couleur avec Paint
+        Point p1, p2;
+        p1 = toPos(mMin); //Il faut définir la méthode toPos
+        p2 = toPos(mMax);
+
+        canvas.drawLine(p1.x, p1.y, p2.x, p2.y, mBarPaint);
+
+        //positionnement du curseur et de la barre d'amplitude
+        Point curseurPosition = toPos(mValue);
+        Point originPosition = toPos(Math.max(0,mMin));
+
+        //On va faire le tracé de notre curseur rond
+        canvas.drawCircle(curseurPosition.x,curseurPosition.y, mCursorDiameter/2, mCursorPaint); //Une fois fait va falloir l'instancier dans le Oncreate
     }
 
     private float dpToPixel(float valueInDp){
         return TypedValue.applyDimension((TypedValue.COMPLEX_UNIT_DIP, valueInDp, getResources().getDisplayMetrics());
     }
 
+    private Point toPos(float value){
+        int x, y;
+        //à présent x1 et y1 représentent le centre du curseur
+        //cette méthode adapte la dimension demandée aux spécifications,
+        // Si la place est disponible, la suggestion est conservée sinon la contrainte est conservée
 
+        y = (int) ((1- valueToRatio(value))* mBarLenght + mCursorDiameter /2);
+        x = (int) (Math.max(mCursorDiameter, mBarWidth)/2);
+        x += getPaddingLeft();
+        y += getPaddingTop();
 
+        return new Point(x,y);
+    }
 
+    //Transforme le min et max de notre curseur en une valeur entre 0 et 1
+    private float valueToRatio(float value){
+        return (value - mMin)/(mMax - mMin);
+    }
 
+    private float ratioToValue(float ratio) {
+        return ratio * (mMax-mMin)+mMin;
+
+    }
 }
